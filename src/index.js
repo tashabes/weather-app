a1c01f12b49eb4729797d06f722ee6341e9a15cb
Natasha Abel <natashaabel@MacBookAir.Home>
2024-12-15 17:15:30 +0000

    updated api key

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function formatDate(timestamp) {
 }
 
 function searchCity(city) {
-  let apiKey = "2b17fbae1b8b000780a0accf16f61402";
+  let apiKey = "ebd1ae53153a500ae937613bb4637989";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
@@ -81,7 +81,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2b17fbae1b8b000780a0accf16f61402";
+  let apiKey = "ebd1ae53153a500ae937613bb4637989";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -126,7 +126,7 @@ function buttonClick(event) {
 }
 
 function retrievePosition(position) {
-  let apiKey = "2b17fbae1b8b000780a0accf16f61402";
+  let apiKey = "ebd1ae53153a500ae937613bb4637989";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
